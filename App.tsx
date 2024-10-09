09c849f5f8d95d942d945d6a963244d35b4aba3f
西巻拓真 <t.nishimaki@MacBook-Pro.elecom>
2024-10-09 21:27:00 +0900

    フッター追加

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import Detail from './src/Detail';
+import SettingsScreen from './src/SettingScreen';
+
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Detail />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === '野菜価格') {
+              iconName = focused ? 'leaf' : 'leaf-outline';
+            } else if (route.name === '設定') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="野菜価格" component={Detail} />
+        <Tab.Screen name="設定" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
 
 export default App;
