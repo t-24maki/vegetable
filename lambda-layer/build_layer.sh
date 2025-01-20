0e2e20cb800bc135cc8ca57fff5cef285d1df194
西巻拓真 <t.nishimaki@Mac.elecom>
2025-01-20 15:46:00 +0900

    ファイル整理

--- lambda-layer/build_layer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Dockerイメージのビルド
-docker build -t lambda-pdf-builder .
-
-# Dockerコンテナの実行とパッケージの作成
-docker run --rm -v $(pwd)/output:/output lambda-pdf-builder
-
-echo "Lambda deployment package has been created in the output directory."
